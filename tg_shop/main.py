b00bbdd5d7a65b0e6c03469d4b76fa8ee91cfa3e
webteacher <40013749+webteacher@users.noreply.github.com>
2024-11-23 20:21:51 +0200

    Add files via upload

--- tg_shop/main.py
+++ tg_shop/main.py
@@ -1,15 +1,32 @@
 from telegram import Update
 import settings
-from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, filters
+from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, filters, ConversationHandler,CallbackQueryHandler
 import db
+
+FULL_NAME, EMAIL, RATING, FEEDBACK = 0,1,2,3
 
 # Функція для обробки команди /start
 async def start(update: Update, context):
-    await update.message.reply_text("Привіт, мене цікавить ваш відгук про наш магазин! Напишіть щось:) ")
+    await update.message.reply_text("Привіт, мене цікавить ваш відгук про наш магазин! Введіть імя:) ")
+    return FULL_NAME
 
-async def echo(update: Update, context):
-    await update.message.reply_text(f"Ви написали: {update.message.text}")
-    application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, echo))
+
+
+async def name(update: Update, context):
+    context.user_data['full_name'] = update.message.text
+    await update.message.reply_text(f"Entry your email:")
+
+    return EMAIL
+
+
+async def email(update: Update, context):
+    context.user_data['email'] = update.message.text
+    await update.message.reply_text(f"Entry rating from 1 to 5:")
+
+    return RATING
+
+
+
 
 
 # async def start(update: Update, context):
@@ -28,6 +45,19 @@ if __name__ == '__main__':
 
     # Додаємо обробник для команди /start
     application.add_handler(CommandHandler('start', start))
+    conv_handler = ConversationHandler(
+        entry_points=[CallbackQueryHandler( start)], #початок реєстрації (після натискання на кнопку "Зареєструватися")
+        states={
+            FULL_NAME: [MessageHandler(filters.TEXT & ~filters.COMMAND, name)], # перше запитання
+            EMAIL: [MessageHandler(filters.TEXT & ~filters.COMMAND, email)],
+            RATING: [MessageHandler(filters.TEXT & ~filters.COMMAND, rating)],
+            FEEDBACK: [MessageHandler(filters.TEXT & ~filters.COMMAND,city )],
+        },
+        fallbacks=[CommandHandler("cancel", cancel)],
+    )
 
+    application.add_handler(conv_handler)
+
+    
     # Запускаємо бота
     application.run_polling()  
--- tg_shop/settings.py
+++ tg_shop/settings.py
@@ -1,1 +1,1 @@
-TOKEN = "7577704246:AAGgrjcOhY_LnqGpSledfm6Xn4nQMbcf8Ro"
+TOKEN = ""
