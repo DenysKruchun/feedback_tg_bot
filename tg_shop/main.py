de53b38b52e2a55bb1a9eafd2717dcb3e67862bb
webteacher <40013749+webteacher@users.noreply.github.com>
2024-12-16 01:01:07 +0200

    Add files via upload

--- tg_shop/main.py
+++ tg_shop/main.py
@@ -12,47 +12,58 @@ FULL_NAME, EMAIL, RATING, FEEDBACK = 0,1,2,3
 # Функція для обробки команди /start
 async def start(update: Update, context):
     await update.message.reply_text("Привіт, мене цікавить ваш відгук про наш магазин! Введіть імя:) ")
+    # await update.message.reply_text(f"{update.message.chat_id}") # отримуєммо ID чату
+
     return FULL_NAME
 
 
 
 async def name(update: Update, context):
-    context.user_data['full_name'] = update.message.text
     name = update.message.text.strip().capitalize()
     if not name.isalpha():
         await update.message.reply_text("Імя повинно бути з літер!:")
         return FULL_NAME
-    
     else:
+        context.user_data['full_name'] = name
         await update.message.reply_text(f"Entry your email:")
         return EMAIL
 
 
 async def email(update: Update, context):
-    context.user_data['email'] = update.message.text
+    
     email = update.message.text.strip()
     if "@" in email:
+        context.user_data['email'] = update.message.text
         await update.message.reply_text(f"Entry rating from 1 to 5:")
         return RATING
-    
     else:
         await update.message.reply_text(f"Invalid email")
         return EMAIL
 
 
 async def rating(update: Update, context):
-    context.user_data['rating'] = update.message.text
     rating = update.message.text.strip()
-    if not int(rating) or rating > 5 or rating < 0:
-        await update.message.reply_text(f"Invalid rating")
-        return RATING
-    else:
+    if rating.isdigit() and int(rating) <=5 and int(rating) > 0:
+        context.user_data['rating'] = update.message.text
         await update.message.reply_text(f"Entry your feedback:")
         return FEEDBACK
+    else:
+        await update.message.reply_text(f"Invalid rating")
+        return RATING 
 
 
 async def feedback(update: Update, context):
     context.user_data['feedback'] = update.message.text
+    await update.message.reply_text("tHANKS FOR REGISTRATION")
+    text = f''' New feedback was created: 
+Ім'я: {context.user_data['full_name']}
+Email:{context.user_data['email']}
+Rating: {context.user_data['rating']}
+Feedback: {context.user_data['feedback']}'''
+    await context.bot.send_message(chat_id = settings.CHAT_ID, text = text  )
+    
+
+
     
 
 #Скасування реєстрації і кінець розмови
@@ -66,15 +77,15 @@ if __name__ == '__main__':
     application = ApplicationBuilder().token(settings.TOKEN).build()
 
     # Додаємо обробник для команди /start
-    application.add_handler(CommandHandler('start', start))
+    # application.add_handler(CommandHandler('start', start))
     conv_handler = ConversationHandler(
-        entry_points=[CallbackQueryHandler(start)], #початок реєстрації (після натискання на кнопку "Зареєструватися")
+        entry_points=[CommandHandler('start', start)], #початок реєстрації (після натискання на кнопку "Зареєструватися")
         states={
             FULL_NAME: [MessageHandler(filters.TEXT & ~filters.COMMAND, name)], # перше запитання
             EMAIL: [MessageHandler(filters.TEXT & ~filters.COMMAND, email)],
             RATING: [MessageHandler(filters.TEXT & ~filters.COMMAND, rating)],
             FEEDBACK: [MessageHandler(filters.TEXT & ~filters.COMMAND, feedback)],
-   
+   https://github.com/volodymyrlogika/tg_feedback/actions
         },
         fallbacks=[CommandHandler("cancel", cancel)],
     )
