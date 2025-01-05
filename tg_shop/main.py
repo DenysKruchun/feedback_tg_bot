60caf6675444fb34e758adc0807fc201aad90617
DenysKruchun <deniskricun44@gmail.com>
2025-01-05 08:45:53 +0200

    add buttons

--- tg_shop/main.py
+++ tg_shop/main.py
@@ -1,4 +1,4 @@
-from telegram import Update
+from telegram import Update,InlineKeyboardButton, InlineKeyboardMarkup
 import settings
 from telegram.ext import(
 ApplicationBuilder, CommandHandler,
@@ -12,6 +12,9 @@ FULL_NAME, EMAIL, RATING, FEEDBACK = 0,1,2,3
 # Функція для обробки команди /start
 async def start(update: Update, context):
     await update.message.reply_text("Привіт, мене цікавить ваш відгук про наш магазин! Введіть імя:) ")
+
+
+    
     # await update.message.reply_text(f"{update.message.chat_id}") # отримуєммо ID чату
 
     return FULL_NAME
@@ -34,33 +37,56 @@ async def email(update: Update, context):
     email = update.message.text.strip()
     if "@" in email:
         context.user_data['email'] = update.message.text
-        await update.message.reply_text(f"Entry rating from 1 to 5:")
+        keyboard = [
+            [InlineKeyboardButton("1", callback_data = "button1")],
+            [InlineKeyboardButton("2", callback_data = "button2")],
+            [InlineKeyboardButton("3", callback_data = "button3")],
+            [InlineKeyboardButton("4", callback_data = "button4")],
+            [InlineKeyboardButton("5", callback_data = "button5")]
+        ]        
+        reply_markup = InlineKeyboardMarkup(keyboard)
+        await update.message.reply_text("Choose your rating!", reply_markup=reply_markup)
+    
+        
+
+
+    
         return RATING
     else:
         await update.message.reply_text(f"Invalid email")
         return EMAIL
+    
+
+        
 
 
-async def rating(update: Update, context):
+async def rating(update: Update, context): #rating
     rating = update.message.text.strip()
+   
     if rating.isdigit() and int(rating) <=5 and int(rating) > 0:
-        context.user_data['rating'] = update.message.text
+        context.user_data['rating'] = update.message.text 
         await update.message.reply_text(f"Entry your feedback:")
-        return FEEDBACK
+        return FEEDBACK 
     else:
         await update.message.reply_text(f"Invalid rating")
         return RATING 
 
+    
+async def button(update: Update, context):
+    query = update.callback_query
+    await query.answer()
+    await query.edit_message_text(text = f"Selected option{query.data}")
+
 
 async def feedback(update: Update, context):
     context.user_data['feedback'] = update.message.text
-    await update.message.reply_text("tHANKS FOR REGISTRATION")
+    await update.message.reply_text("THANKS FOR REGISTRATION")
     text = f''' New feedback was created: 
 Ім'я: {context.user_data['full_name']}
 Email:{context.user_data['email']}
 Rating: {context.user_data['rating']}
 Feedback: {context.user_data['feedback']}'''
-    await context.bot.send_message(chat_id = settings.CHAT_ID, text = text  )
+    await context.bot.send_message(chat_id = settings.CHAT_ID, text = text )
     
 
 
@@ -75,22 +101,26 @@ async def cancel(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
 # Основна частина програми
 if __name__ == '__main__':
     application = ApplicationBuilder().token(settings.TOKEN).build()
+    
 
     # Додаємо обробник для команди /start
-    # application.add_handler(CommandHandler('start', start))
+    application.add_handler(CallbackQueryHandler(rating))
+
     conv_handler = ConversationHandler(
-        entry_points=[CommandHandler('start', start)], #початок реєстрації (після натискання на кнопку "Зареєструватися")
+        entry_points=[CommandHandler('start', start)],
+         #початок реєстрації (після натискання на кнопку "Зареєструватися")
         states={
             FULL_NAME: [MessageHandler(filters.TEXT & ~filters.COMMAND, name)], # перше запитання
             EMAIL: [MessageHandler(filters.TEXT & ~filters.COMMAND, email)],
             RATING: [MessageHandler(filters.TEXT & ~filters.COMMAND, rating)],
             FEEDBACK: [MessageHandler(filters.TEXT & ~filters.COMMAND, feedback)],
-   https://github.com/volodymyrlogika/tg_feedback/actions
+#    https://github.com/volodymyrlogika/tg_feedback/actions
         },
         fallbacks=[CommandHandler("cancel", cancel)],
     )
 
     application.add_handler(conv_handler)
+    application.add_handler(CallbackQueryHandler(button))
 
     
     # Запускаємо бота
